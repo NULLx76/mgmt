b2723584df63af974cb2b6c84cc07cb0c66f1f22
Victor Roest <victor@xirion.net>
2017-11-29 17:56:37 +0100

    Changed to correct file

---
+++ client/mgmt_client/apt.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+import apt
+
+cache=apt.Cache()
+cache.update()
+cache.open(None)
+cache.upgrade()
--- client/mgmt_client/core.py
+++ client/mgmt_client/core.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python
-#coding=utf-8
-
-import apt
-
-cache=apt.Cache()
-cache.update()
-cache.open(None)
-cache.upgrade()
 
 
 
