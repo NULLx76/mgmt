24e4f2156e3b966873548ec57a3918dd47998d10
Victor Roest <victor@xirion.net>
2017-11-29 18:41:40 +0100

    Added check for ubuntu

--- client/mgmt_client/core.py
+++ client/mgmt_client/core.py
@@ -1,3 +1,9 @@
-import ubuntu_apt
-print (ubuntu_apt.get_update_packages())
+import platform
+import subprocess
+
+if platform.system() == "Linux" and subprocess.check_output(["lsb_release", "-is"]) == "Ubuntu":
+    import ubuntu_apt
+    print(ubuntu_apt.get_update_packages())
+
+
 
