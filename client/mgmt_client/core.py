3464f859a3aed6aa4927e289c273e9c048c67ad6
Victor Roest <victor@xirion.net>
2017-11-29 18:45:56 +0100

    Fixed check

--- client/mgmt_client/core.py
+++ client/mgmt_client/core.py
@@ -1,7 +1,7 @@
 import platform
 import subprocess
 
-if platform.system() == "Linux" and subprocess.check_output(["lsb_release", "-is"]) == "Ubuntu":
+if platform.system() == "Linux" and "Ubuntu" in subprocess.check_output(["lsb_release", "-is"]).decode():
     import ubuntu_apt
     print(ubuntu_apt.get_update_packages())
 
