f293ef642d32da74bcef5e6d3f591b36f449865d
Victor Roest <victor@xirion.net>
2017-11-29 21:13:40 +0100

    testing

--- client/mgmt_client/core.py
+++ client/mgmt_client/core.py
@@ -1,13 +1,11 @@
 import platform
 import subprocess
-import json
 
 if platform.system() == "Linux" and "Ubuntu" in subprocess.check_output(["lsb_release", "-is"]).decode():
     import ubuntu_apt
     pkgs = ubuntu_apt.get_update_packages()
     updates = len(pkgs)
     security_updates = len([x for x in pkgs if x.get('security')])
-    print(json.dumps(pkgs))
-
+    print(type(pkgs))
 else:
     print("Sorry this platform is not supported")
