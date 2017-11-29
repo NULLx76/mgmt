3c502e22cc901e1026339160c888ef9eaddbca51
Victor Roest <victor@xirion.net>
2017-11-29 21:34:45 +0100

    First go version

--- client/mgmt_client/core.py
+++ client/mgmt_client/core.py
@@ -1,11 +1,12 @@
 import platform
 import subprocess
+import json
 
 if platform.system() == "Linux" and "Ubuntu" in subprocess.check_output(["lsb_release", "-is"]).decode():
     import ubuntu_apt
     pkgs = ubuntu_apt.get_update_packages()
     updates = len(pkgs)
     security_updates = len([x for x in pkgs if x.get('security')])
-    print(type(pkgs))
+    msg = json.dumps(pkgs)
 else:
     print("Sorry this platform is not supported")
