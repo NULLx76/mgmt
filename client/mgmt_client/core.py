52b66fb233c1a6fe5f765ea6ea66d9ec38e9c752
Victor Roest <victor@xirion.net>
2017-12-03 12:59:31 +0100

    Added better comments

--- client/mgmt_client/core.py
+++ client/mgmt_client/core.py
@@ -5,6 +5,7 @@ import pymysql as mysql
 import uuid
 import socket
 
+# Get Hardware UUID (Mac address in decimal)
 uuid = uuid.getnode()
 
 # Get ip
@@ -24,22 +25,24 @@ else:
 # Connect to Database
 conn = mysql.connect(host='mariadb', port=3306, user='mgmt', db='mgmt', password='mgmt_pass')
 
-# Check if in Database
+# Check if system exists in DB, if not the case then it will add it
 try:
     with conn.cursor() as cursor:
         # Inventory Table
-        sqlCheckInventory = "SELECT * from mgmt.inventory WHERE MAC=%s"
+        sqlCheckInventory = "SELECT 1 from mgmt.inventory WHERE MAC=%s LIMIT 1"
         cursor.execute(sqlCheckInventory, str(uuid))
 
+        # If not in the inventory table add it
         if cursor.fetchone() is None:
             print("Adding to inventory")
             sqlAddToInventory = "INSERT INTO mgmt.inventory (MAC, IP, Hostname,OS) VALUES(%s, INET_ATON(%s), %s, %s)"
             cursor.execute(sqlAddToInventory, (str(uuid), str(ip), hostname, os))
 
         # Updates Table
-        sqlCheckUpdates = "SELECT * from mgmt.updates WHERE MAC=%s"
+        sqlCheckUpdates = "SELECT 1 from mgmt.updates WHERE MAC=%s LIMIT 1"
         cursor.execute(sqlCheckUpdates, str(uuid))
 
+        # If not in the Updates table add it
         if cursor.fetchone() is None:
             print("Adding to Updates")
             sqlAddToUpdates = "INSERT INTO mgmt.updates (MAC) VALUES(%s)"
@@ -53,12 +56,15 @@ except Exception as e:
 if "Ubuntu" in os:
     import ubuntu_apt
     fullcache = ubuntu_apt.get_update_packages()
-    pkgs = []
+
     updates = len(fullcache)
     security_updates = len([x for x in fullcache if x.get('security')])
+
+    pkgs = []
     for pkg in fullcache:
         pkgs.append(pkg.get('name'))
 
+    # Pushes data to the mysql database
     try:
         with conn.cursor() as cursor:
             sqlUpdateUpdates = "UPDATE mgmt.updates " \
@@ -69,9 +75,11 @@ if "Ubuntu" in os:
                            (str(updates), str(security_updates), str(pkgs), str(fullcache), str(uuid)))
 
         conn.commit()
-    finally:
-        conn.close()
-
+    except Exception as e:
+        print(e)
 
 else:
     print("Sorry this platform is not supported")
+
+# Close mysql connection
+conn.close()
